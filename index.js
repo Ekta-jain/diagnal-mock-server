91dbb2cc413a8905cf7e1887ae93863c69ea319a
Ekta Jain <jainekta9876@gmail.com>
2023-01-02 18:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT;
 
 app.get('/records', (req, res) => {
     // console.log(req.query);
